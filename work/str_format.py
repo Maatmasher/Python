cc0a73e414b8a1a42c1273aa12d28c9080019d26
Maatmasher <yakushin.nikolay@gmail.com>
2025-03-30 01:00:52 +1000

    1st commit

---
+++ work/str_format.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+age = 49
+name = 'Nikolay'
+
+print('Возраст {0} -- {1} лет.'.format(name, age))
+print('Почему  {0} пытается изучить этот Python?'.format(name))
---
+++ work/var.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# File Name : var.py
+
+i =5
+print(i)
+i = i + 1
+print(i)
+
+s = '''Это многострочная строка.
+Это вторая её строчка.'''
+print(s)
