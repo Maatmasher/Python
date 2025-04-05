e2b17ac7a76570e29caf55253df0123bbf06d3ea
Maatmasher <yakushin.nikolay@gmail.com>
2025-04-06 01:54:10 +1000

    Continue first step

---
+++ work/Question.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+i = 1
+
+while False:
+    if i % 2 ==0:
+        
+        break
+
+    print(i)
+
+    i += 2
---
+++ work/expression.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+length = 5
+breadth = 2
+
+area = length * breadth
+print('Плащадь равна',area)
+print('Периметр равен', 2*(length + breadth))
