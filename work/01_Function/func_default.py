bee1a20ebe393b3f1e89b1b837a298106e7b00b2
Maatmasher <yakushin.nikolay@gmail.com>
2025-04-20 15:10:35 +0300

    Ожидал время

---
+++ work/01_Function/func_default.py
@@ -0,0 +1,6 @@
+# !/usr/bin/env python3
+def say(message, times = 1):
+    print(message * times)
+
+say('Привет')
+say('Мир', 5)
---
+++ work/01_Function/func_key.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def func(a, b=5, c=10):
+    print('а равно', a,', b равно', b, ', а c равно', c)
+
+func(3, 7)
+func(25, c=24)
+func(c=50, a=100)
