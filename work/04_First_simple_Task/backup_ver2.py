268482d77aa62aafdd058fedd41fe6ce843102ef
Maatmasher <yakushin.nikolay@gmail.com>
2025-05-01 10:36:40 +1000

    change

--- work/04_First_simple_Task/backup_ver2.py
+++ work/04_First_simple_Task/backup_ver2.py
@@ -2,7 +2,8 @@
 
 import os
 import time
-# 1. Файлы и каталоги, которые необходимо скопировать, собираются в список.
+
+# 1. Файлы и каталоги, которые необходимо скопировать, собираются в список
 if os.name == 'nt':
     source = ['C:\\Users\Coldfear\Documents\\"SQUARE ENIX"', 'C:\\Users\Coldfear\Documents\py']
 elif os.name == 'posix':
@@ -10,11 +11,10 @@ elif os.name == 'posix':
 else:
     print('Неизвестная  OS для скрипта, остановка')
     exit
-# Заметьте, что для имён, содержащих пробелы, необходимо использовать
+# Для имён, содержащих пробелы, необходимо использовать
 # двойные кавычки внутри строки.
 
 # 2. Резервные копии должны храниться в основном каталоге резерва.
-# Подставьте тот путь, который вы будете использовать.
 if os.name == 'nt':
     target_dir = 'D:\\PythonScript' 
 elif os.name == 'posix':
@@ -24,11 +24,22 @@ else:
     exit
 
 # 3. Файлы помещаются в zip-архив.
-# 4. Именем для zip-архива служит текущая дата и время.
-target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+# 4. Текущая дата служит именем подкаталога в основном каталоге
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+# Текущее время служит именем zip-архива
+now = time.strftime('%H%M%S')
+
+# Создаём каталог, если его ещё нет
+if not os.path.exists(today):
+    os.mkdir(today) # создание каталога
+    print('Каталог успешно создан', today)
+
+# Имя zip-файла
+target = today+os.sep+now+'.zip'
+
 # 5. Используем команду "zip" для помещения файлов в zip-архив
-zip_command = "zip -qr {0} {1}".format(target, ' '.join(source)) 
-print(zip_command)
+zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
+
 # Запускаем создание резервной копии
 if os.system(zip_command) == 0:
     print('Резервная копия успешно создана в', target)
