66c141375b4ebe4a3b78de50cf64e62e2c68f042
Maatmasher <yakushin.nikolay@gmail.com>
2025-05-06 11:35:13 +1000

    extension

--- work/01_Function/func_key.py
+++ work/01_Function/func_key.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 def func(a, b=5, c=10):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (int, optional): _description_. Defaults to 5.
+        c (int, optional): _description_. Defaults to 10.
+    """
     print('а равно', a,', b равно', b, ', а c равно', c)
 
 func(3, 7)
