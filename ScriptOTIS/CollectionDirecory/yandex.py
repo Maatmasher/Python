802f0e8be3b0a9f81180cb20c49b32a46313aec7
Maatmasher <yakushin.nikolay@gmail.com>
2025-07-09 18:21:46 +1000

    09.07.2025_3

--- ScriptOTIS/CollectionDirecory/yandex.py
+++ ScriptOTIS/CollectionDirecory/yandex.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import requests
 from time import sleep
+import certifi
+import os
 
-API_KEY = "7ce8ab1a-0ee9-4e2d-abc2-53bb3e6a6532"  # Замените на реальный ключ
-INPUT_FILE = r"C:\Users\iakushin.n\Documents\GitHub\Python\ScriptOTIS\CollectionDirecory\yandex.xlsx"
-OUTPUT_FILE = r"C:\Users\iakushin.n\Documents\GitHub\Python\ScriptOTIS\CollectionDirecory\coordinates_output.xlsx"
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+INPUT_FILE = os.path.join(CURRENT_DIR, "yandex.xlsx")
+OUTPUT_FILE = os.path.join(CURRENT_DIR, "coordinates_output.xlsx")
+API_KEY = "7ce8ab1a-0ee9-4e2d-abc2-53bb3e6a6532"  # Нужен ключ разработчика API Яндекс
+
 
 def geocode_yandex(address):
+    """По адресам в eXel получаем геоданные"""
     url = "https://geocode-maps.yandex.ru/1.x/"
     params = {
         "apikey": API_KEY,
@@ -14,28 +19,28 @@ def geocode_yandex(address):
         "format": "json",
     }
     try:
-        # Отключаем проверку SSL (только для теста!)
-        response = requests.get(url, params=params, verify=False)
+        response = requests.get(url, params=params, verify=certifi.where())
         data = response.json()
-        pos = data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
+        pos = data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"][
+            "Point"
+        ]["pos"]
         lon, lat = pos.split(" ")
         return lat, lon
     except Exception as e:
         print(f"Ошибка: {address} – {e}")
         return None, None
 
-# Чтение файла
+
+# Чтение и обработка файла
 df = pd.read_excel(INPUT_FILE, header=None, names=["address"])
 df["latitude"] = None
 df["longitude"] = None
 
-# Обработка адресов
 for index, row in df.iterrows():
     lat, lon = geocode_yandex(row["address"])
     df.at[index, "latitude"] = lat
     df.at[index, "longitude"] = lon
-    sleep(0.3)  # Задержка для API
+    sleep(1)  # Задержка запросов или Яндекс ключ забанит
 
-# Сохранение
 df.to_excel(OUTPUT_FILE, index=False)
-print("Готово! Результат сохранен в", OUTPUT_FILE)
+print("Готово! Результат сохранён в", OUTPUT_FILE)
